ba1c4475cb255e932faa891df9c90279d36a348b
Jyotikri30 <jyoti30505@gmail.com>
2026-04-08 20:37:08 +0530

    Refactor tic-tac-toe AI

--- tic_tac_toe_ai.py/tic_tac.py
+++ tic_tac_toe_ai.py/tic_tac.py
@@ -4,7 +4,6 @@ import random
 player_score = 0
 computer_score = 0
 
-# Show position guide
 def show_guide():
     print("\nPosition Guide:")
     print("1 | 2 | 3")
@@ -13,7 +12,7 @@ def show_guide():
     print("---------")
     print("7 | 8 | 9\n")
 
-# Print board
+
 def print_board(board):
     print("\n")
     print(board[0] + " | " + board[1] + " | " + board[2])
@@ -23,7 +22,6 @@ def print_board(board):
     print(board[6] + " | " + board[7] + " | " + board[8])
     print("\n")
 
-# Check winner
 def check_winner(board, player):
     win_conditions = [
         [0,1,2],[3,4,5],[6,7,8],
@@ -35,7 +33,7 @@ def check_winner(board, player):
             return True
     return False
 
-# Player move
+
 def player_move(board):
     move = int(input("Enter your move (1-9): ")) - 1
     if board[move] == " ":
@@ -44,14 +42,14 @@ def player_move(board):
         print("Already taken!")
         player_move(board)
 
-# Easy AI
+
 def easy_move(board):
     moves = [i for i in range(9) if board[i] == " "]
     return random.choice(moves)
 
-# Hard AI (basic smart)
+
 def hard_move(board):
-    # Try to win
+    
     for i in range(9):
         if board[i] == " ":
             board[i] = "O"
@@ -60,7 +58,7 @@ def hard_move(board):
                 return i
             board[i] = " "
 
-    # Block player
+
     for i in range(9):
         if board[i] == " ":
             board[i] = "X"
@@ -71,7 +69,7 @@ def hard_move(board):
 
     return easy_move(board)
 
-# Main game
+# Main prog
 while True:
     board = [" "] * 9
 
@@ -86,7 +84,7 @@ while True:
 
         if check_winner(board, "X"):
             print_board(board)
-            print("You win! 🎉")
+            print("You win! ")
             player_score += 1
             break
 
@@ -94,7 +92,7 @@ while True:
             print("Draw!")
             break
 
-        # Computer move
+        # Computer prog
         if level == "hard":
             move = hard_move(board)
         else:
@@ -105,7 +103,7 @@ while True:
 
         if check_winner(board, "O"):
             print_board(board)
-            print("Computer wins! 🤖")
+            print("Computer wins! ")
             computer_score += 1
             break
 
@@ -117,5 +115,5 @@ while True:
 
     replay = input("Play again? (y/n): ").lower()
     if replay != "y":
-        print("Thanks for playing! 👋")
+        print("Thanks for playing! ")
         break
